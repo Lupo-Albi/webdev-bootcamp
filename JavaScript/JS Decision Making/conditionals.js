99222e95ee1c52351824882aad487e8475575ce3
Lupo-Albi <luandersonalbuquerque@gmail.com>
2020-12-01 20:00:09 -0300

    if statement

---
+++ JavaScript/JS Decision Making/conditionals.js
@@ -0,0 +1,13 @@
+console.log("BEFORE CONDITIONAL");
+let random = Math.random();
+
+if (random < 0.5) {
+    console.log("Your number is less than 0.5!");
+    console.log(random);
+}
+
+if (random >= 0.5) {
+    console.log("Your number is greater than 0.5!");
+    console.log(random);
+}
+console.log("AFTER CONDITIONAL");
