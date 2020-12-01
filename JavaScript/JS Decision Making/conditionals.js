d34b0b0d5c6ef080823a2ab939b8765cca2fc2ac
Lupo-Albi <luandersonalbuquerque@gmail.com>
2020-12-01 20:23:44 -0300

    else statement

--- JavaScript/JS Decision Making/conditionals.js
+++ JavaScript/JS Decision Making/conditionals.js
@@ -3,26 +3,27 @@
 
 // if (random < 0.5) {
 //     console.log("Your number is less than 0.5!");
-//     console.log(random);
-// }
-
-// if (random >= 0.5) {
+// } else {
 //     console.log("Your number is greater than 0.5!");
-//     console.log(random);
 // }
+// console.log(random);
 // console.log("AFTER CONDITIONAL");
 
 //======================================================
 
-// const dayOfWeek = 'Monday';
+// const dayOfWeek = prompt("Enter a day").toLowerCase();
 
-// if (dayOfWeek === 'Monday') {
+// if (dayOfWeek === "monday") {
 //     console.log("UGH I HATE MONDAYS!");
-// } else if (dayOfWeek === 'Saturday') {
-//     console.log('Yay I love Saturdays!');
-// } else if (dayOfWeek === 'Friday') {
-//     console.log('Fridays are decent, especially after work!');
+// } else if (dayOfWeek === 'saturday') {
+//     console.log("Yay I love Saturdays!");
+// } else if (dayOfWeek === "friday") {
+//     console.log("Fridays are decent, especially after work!");
+// } else {
+//     console.log("MEH");
 // }
+
+//===============================================
 
 // 0 - 5 - Free
 // 5 - 10 Child $10
@@ -36,6 +37,6 @@ if (age < 5) {
     console.log("You are a child. You pay $10!");
 } else if (age < 65) {
     console.log("You are an adult. You pay $20!");
-} else if (age >= 65) {
+} else {
     console.log("You are a senior. You pay $10!");
 }
