3c803bb873a26afdce91d91c4db42d2b7d0386b1
Lupo-Albi <luandersonalbuquerque@gmail.com>
2020-12-01 20:37:02 -0300

    nesting conditionals

--- JavaScript/JS Decision Making/conditionals.js
+++ JavaScript/JS Decision Making/conditionals.js
@@ -29,14 +29,31 @@
 // 5 - 10 Child $10
 // 10 - 65 Adult $20
 // 65+ Senior $10
-const age = 18;
+// const age = 18;
 
-if (age < 5) {
-    console.log("You are a baby. You get in for free!");
-} else if (age < 10) {
-    console.log("You are a child. You pay $10!");
-} else if (age < 65) {
-    console.log("You are an adult. You pay $20!");
+// if (age < 5) {
+//     console.log("You are a baby. You get in for free!");
+// } else if (age < 10) {
+//     console.log("You are a child. You pay $10!");
+// } else if (age < 65) {
+//     console.log("You are an adult. You pay $20!");
+// } else {
+//     console.log("You are a senior. You pay $10!");
+// }
+
+//==========================================================
+
+const password = prompt("Please enter a new password");
+
+// Password must be 6+ characters
+// Password cannot include space
+
+if (password.length >= 6) {
+    if (password.indexOf(' ') === -1 ) {
+        console.log("Valid Password!");
+    } else {
+        console.log("Password cannot contain spaces!");
+    }
 } else {
-    console.log("You are a senior. You pay $10!");
+    console.log("Password too short! Must be 6+ characters");
 }
