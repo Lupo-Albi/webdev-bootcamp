1d7d239f17f4267801d7eb68e29eba0c330f0d22
Lupo-Albi <luandersonalbuquerque@gmail.com>
2020-12-01 21:09:24 -0300

    logical NOT

--- JavaScript/JS Decision Making/Logical Operators/app.js
+++ JavaScript/JS Decision Making/Logical Operators/app.js
@@ -6,14 +6,26 @@
 //     console.log("Incorrect format for password!");
 // }
 
-const age = prompt("Enter your age");
+// const age = prompt("Enter your age");
 
-if ( age > 0 && age < 5 || age >= 65) {
-    console.log("Free");
-} else if ( age >= 5 && age < 10) {
-    console.log("$10");
-} else if ( age >= 10 && age < 65) {
-    console.log("$20");
-} else {
-    console.log("Invalid age");
+// if ( age > 0 && age < 5 || age >= 65) {
+//     console.log("Free");
+// } else if ( age >= 5 && age < 10) {
+//     console.log("$10");
+// } else if ( age >= 10 && age < 65) {
+//     console.log("$20");
+// } else {
+//     console.log("Invalid age");
+// }
+
+// const firstName = prompt("Enter your first name:");
+
+// if (!firstName) {
+//     firstName = prompt("Try again!");
+// }
+
+const age = 45;
+
+if (!(age > 0 && age < 5 || age >= 65)) {
+    console.log("You are not a baby or a senior!");
 }
