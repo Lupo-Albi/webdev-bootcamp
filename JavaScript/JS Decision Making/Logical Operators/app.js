092f2759eb548cca64f986528a88919c3dfb591b
Lupo-Albi <luandersonalbuquerque@gmail.com>
2020-12-01 21:05:13 -0300

    logicar OR

--- JavaScript/JS Decision Making/Logical Operators/app.js
+++ JavaScript/JS Decision Making/Logical Operators/app.js
@@ -1,7 +1,19 @@
-const password = prompt("Enter your password");
+// const password = prompt("Enter your password");
 
-if(password.length >= 6 && password.indexOf(' ') === -1) {
-    console.log("Valid password!");
+// if(password.length >= 6 && password.indexOf(' ') === -1) {
+//     console.log("Valid password!");
+// } else {
+//     console.log("Incorrect format for password!");
+// }
+
+const age = prompt("Enter your age");
+
+if ( age > 0 && age < 5 || age >= 65) {
+    console.log("Free");
+} else if ( age >= 5 && age < 10) {
+    console.log("$10");
+} else if ( age >= 10 && age < 65) {
+    console.log("$20");
 } else {
-    console.log("Incorrect format for password!");
+    console.log("Invalid age");
 }
