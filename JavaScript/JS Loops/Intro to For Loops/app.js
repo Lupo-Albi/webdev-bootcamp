adc33e7ccf4809366ada2d254c01f803712c20ec
Lupo-Albi <luandersonalbuquerque@gmail.com>
2020-12-04 19:06:37 -0300

    intro to for loops

---
+++ JavaScript/JS Loops/Intro to For Loops/app.js
@@ -0,0 +1,19 @@
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+// console.log(9)
+// console.log(10)
+
+for (let num = 1; num <= 10; num++) {
+    console.log(num);
+}
+
+// Print out "Da ba dee da ba daa" 6 times, using a for loop
+for (let i = 0; i < 6; i++) {
+    console.log("Da ba dee da ba daa");
+}
