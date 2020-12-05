e85aee61c134243614291abc752ce6616924c197
Lupo-Albi <luandersonalbuquerque@gmail.com>
2020-12-05 15:27:22 -0300

    nested loops

---
+++ JavaScript/JS Loops/Nested Loops/app.js
@@ -0,0 +1,30 @@
+// let str = 'LOL';
+
+// for (let i = 0; i <= 4; i++) {
+//     console.log("Outer:", i);
+//     for (let j = 0; j < str.length; j++) {
+//         console.log('   Inner:', str[j]);
+//     }
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`i is: ${i}`);
+//     for (let j = 1; j < 4; j++) {
+//         console.log(`   j is: ${j}`);
+//     }
+// }
+
+const seatingChart = [
+    [ 'Kriste', 'Erik', 'Namita'],
+    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+    ['Yuma', 'Sakura', 'Jack', 'Erika']
+]
+
+for (let i = 0; i < seatingChart.length; i++) {
+    const row = seatingChart[i];
+    console.log(`Row #${i + 1}`)
+
+    for (let j = 0; j < row.length; j++) {
+        console.log(`   ${row[j]}`);
+    }
+}
