1dab63885be784a85035f3d1810176dd9a11f551
Lupo-Albi <luandersonalbuquerque@gmail.com>
2020-12-06 15:36:46 -0300

    returning functions

--- JavaScript/JS Leveling Up Functions/High Order Functions/app.js
+++ JavaScript/JS Leveling Up Functions/High Order Functions/app.js
@@ -19,3 +19,41 @@ function rollDie(){
 }
 
 callTwice(rollDie);
+
+function makeMysteryFunc() {
+    const rand = Math.random();
+    if (rand > 0.5) {
+        return function() {
+            console.log("Congrats, I am a good functio!n")
+            console.log("You win a million dollar!")
+        }
+    } else {
+        return function() {
+            alert("YOU HAVE BEEN INFECTED BY A COMPUTER VIRUS!")
+            alert("STOP TRYING TO CLOSE THIS WINDOW!")
+            alert("STOP TRYING TO CLOSE THIS WINDOW!")
+            alert("STOP TRYING TO CLOSE THIS WINDOW!")
+            alert("STOP TRYING TO CLOSE THIS WINDOW!")
+            alert("STOP TRYING TO CLOSE THIS WINDOW!")
+        }
+    }
+}
+
+// function isBetween(num) {
+//     return num >= 50 && num <= 100
+// }
+
+// function isBetween2(num) {
+//     return num >= 1 && num <= 10
+// }
+
+function makeBetweenFunc(min, max) {
+    return function(num) {
+        return num >= min && num <= max
+    }
+
+}
+
+const isChild = makeBetweenFunc(0, 17);
+const isAdult = makeBetweenFunc(18, 63);
+const isSenior = makeBetweenFunc(64, 120);
