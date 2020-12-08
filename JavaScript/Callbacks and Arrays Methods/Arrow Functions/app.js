27dc41499d631a32bf7d34a96952634a278d0928
Lupo-Albi <luandersonalbuquerque@gmail.com>
2020-12-08 13:57:54 -0300

    arrow functions implict return

--- JavaScript/Callbacks and Arrays Methods/Arrow Functions/app.js
+++ JavaScript/Callbacks and Arrays Methods/Arrow Functions/app.js
@@ -9,14 +9,25 @@ const sum = (x, y) => {
     return x + y;
 }
 
+// Use empty parens for functions without parameters
 const rollDie = () => {
     return Math.floor(Math.random * 6) + 1;
 }
 
+// parens are optional if there is only one parameter:
 const isEven = num => {
-    if (num % 2 === 0) {
-        return true;
-    } else {
-        return false;
-    }
+    return num % 2 === 0;
 }
+
+// Implicit Return: only with arrow functions
+// Only works with a single expression in the body of the function
+const isEven = num => (
+    num % 2 === 0
+);
+const isEven = num => num % 2 === 0;// one-liner implicit return
+
+const rollDie = () => (
+    Math.floor(Math.random * 6) + 1
+);
+
+const add = (a, b) => a + b;
