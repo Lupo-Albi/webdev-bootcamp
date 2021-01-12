e9ef94b10aa713ef43169cc877f025c1ca30fed7
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-01-12 14:18:56 -0300

    querySelector and querySelectorAll

--- DOM/Introducing the World of The DOM/DOM Intro Code/Manipulating/app.js
+++ DOM/Introducing the World of The DOM/DOM Intro Code/Manipulating/app.js
@@ -1,3 +1,22 @@
+// getElementsByTagName
+// const allImages = document.getElementsByTagName('img');
+// for (let image of allImages) {
+//     console.log(image.src)
+//     image.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Partridge_Silkie_hen.jpg/900px-Partridge_Silkie_hen.jpg'
+// }
+
+// getElementsByClassName
+// const squareImages = document.getElementsByClassName('square');
+// for (let image of squareImages) {
+//     image.src = 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e0/Male_Silkie.png/460px-Male_Silkie.png'
+// }
+
+const links = document.querySelectorAll('p a');
+
+for (let link of links) {
+    console.log(link.href);
+}
+
 const allLinks = document.querySelectorAll('a');
 
 // for (let link of allLinks) {
@@ -9,15 +28,4 @@ for (let link of allLinks) {
     link.style.color = 'rgb(0, 108, 134)';
     link.style.textDecorationColor = 'magenta';
     link.style.textDecorationStyle = 'wavy'
-}
-
-// const allImages = document.getElementsByTagName('img');
-
-// for (let image of allImages) {
-//     console.log(image.src)
-//     image.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Partridge_Silkie_hen.jpg/900px-Partridge_Silkie_hen.jpg'
-// }
-const squareImages = document.getElementsByClassName('square');
-for (let image of squareImages) {
-    image.src = 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e0/Male_Silkie.png/460px-Male_Silkie.png'
 }
