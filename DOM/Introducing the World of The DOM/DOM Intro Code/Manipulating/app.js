1a56f4985b18f19ac3934f48f53adf374f460f1c
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-01-12 14:06:32 -0300

    getElementsByClassName and getElementsByTagName

--- DOM/Introducing the World of The DOM/DOM Intro Code/Manipulating/app.js
+++ DOM/Introducing the World of The DOM/DOM Intro Code/Manipulating/app.js
@@ -10,3 +10,14 @@ for (let link of allLinks) {
     link.style.textDecorationColor = 'magenta';
     link.style.textDecorationStyle = 'wavy'
 }
+
+// const allImages = document.getElementsByTagName('img');
+
+// for (let image of allImages) {
+//     console.log(image.src)
+//     image.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Partridge_Silkie_hen.jpg/900px-Partridge_Silkie_hen.jpg'
+// }
+const squareImages = document.getElementsByClassName('square');
+for (let image of squareImages) {
+    image.src = 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e0/Male_Silkie.png/460px-Male_Silkie.png'
+}
