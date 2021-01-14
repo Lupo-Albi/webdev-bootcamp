bd3779127ddc368e52806a1b8da91819dd5497a4
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-01-13 21:05:37 -0300

    selecting

--- DOM/Introducing the World of The DOM/DOM Intro Code/Selecting/app.js
+++ DOM/Introducing the World of The DOM/DOM Intro Code/Selecting/app.js
@@ -1,18 +1,21 @@
+// getElementsByTagName
 const allImages = document.getElementsByTagName('img');
 
-for (let img of allImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
+for (let image of allImages) {
+    // console.log(image.src)
+    image.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Partridge_Silkie_hen.jpg/900px-Partridge_Silkie_hen.jpg'
 }
 
-
+// getElementsByClassName
 const squareImages = document.getElementsByClassName('square');
 
-for (let img of squareImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
+for (let image of squareImages) {
+    image.src = 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e0/Male_Silkie.png/460px-Male_Silkie.png'
 }
 
+// querySelectorAll
 const links = document.querySelectorAll('p a');
 
 for (let link of links) {
-    console.log(link.href)
+    console.log(link.href);
 }
