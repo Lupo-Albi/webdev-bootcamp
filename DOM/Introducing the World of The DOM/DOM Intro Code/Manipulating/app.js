079ebb9348e45b2460b8765596641c94f3c11d50
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-01-13 21:33:52 -0300

    changing styles

--- DOM/Introducing the World of The DOM/DOM Intro Code/Manipulating/app.js
+++ DOM/Introducing the World of The DOM/DOM Intro Code/Manipulating/app.js
@@ -4,3 +4,22 @@ const allLinks = document.querySelectorAll('a');
 for (let link of allLinks) {
     link.innerText = "I'M A LINK!"
 }
+
+// Changing styles
+const h1 = document.querySelector('h1');
+h1.style.color = 'green';
+h1.style.fontSize = '3em';
+h1.style.border = '2px solid pink';
+// These are inline style changes
+// Doing it one at a time is annoying
+// Inline styles changes are not ideal
+
+for (let link of allLinks) {
+    link.style.color = 'rgb(0, 108, 134)';
+    link.style.textDecorationColor = 'magenta';
+    link.style.textDecorationStyle = 'wavy';
+}
+
+console.log(window.getComputedStyle(h1).color);
+console.log(window.getComputedStyle(h1).fontFamily);
+console.log(window.getComputedStyle(h1).margin);
