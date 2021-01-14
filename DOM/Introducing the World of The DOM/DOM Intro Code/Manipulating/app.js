bcd01d972c93342279e759c6f7f4a1316c2d43f5
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-01-14 19:55:00 -0300

    append and appendChild

--- DOM/Introducing the World of The DOM/DOM Intro Code/Manipulating/app.js
+++ DOM/Introducing the World of The DOM/DOM Intro Code/Manipulating/app.js
@@ -37,3 +37,32 @@ h2.classList.toggle('purple'); // toggle class purple to false
 console.log(h2.classList);
 h2.classList.toggle('purple'); // toggle class purple to true
 console.log(h2.classList);
+
+// appendChild
+const newImg = document.createElement('img');
+newImg.src = 'https://images.unsplash.com/photo-1517849845537-4d257902454a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80';
+document.body.appendChild(newImg);
+newImg.classList.add('square');
+
+const newH3 = document.createElement('h3');
+newH3.innerText = 'I am new!';
+document.body.appendChild(newH3);
+
+// append
+const p = document.querySelector('p');
+p.append('i am new text yayyy!!!', 'ASDASDASDSADASDS');
+
+// prepend
+const newB = document.createElement('b')
+newB.append('HI!');
+p.prepend(newB);
+
+// insertAdjacentElement
+const newH2 = document.createElement('h2');
+newH2.append('Are Adorable chickens')
+h1.insertAdjacentElement('afterend', newH2);
+
+// after
+const h3 = document.createElement('h3');
+h3.innerText = 'I am h3'
+h1.after(h3);
