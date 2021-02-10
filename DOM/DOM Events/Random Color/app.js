37f84a76dd104a229fbaf383c8cd98126d9a7c88
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-02-10 16:32:16 -0300

    changing h1 text color according to the background color

--- DOM/DOM Events/Random Color/app.js
+++ DOM/DOM Events/Random Color/app.js
@@ -2,13 +2,22 @@ const button = document.querySelector('button');
 const h1 = document.querySelector('h1');
 button.addEventListener('click', function() {
     const newColor = makeRandColor();
-    document.body.style.backgroundColor = newColor;
-    h1.innerText = newColor;
+    const textColor = `rgb(${newColor})`;
+    document.body.style.backgroundColor = textColor;
+    h1.innerText = textColor;
+    h1.style.color = isDarkColor(newColor) ? 'white' : 'black';
 })
 
 const makeRandColor = () => {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-    return `rgb(${r}, ${g}, ${b})`;
+    const rgb = [r, g, b];
+    return rgb;
+}
+
+function isDarkColor(color) {
+    // https://www.w3.org/TR/AERT/#color-contrast
+    const brightness = Math.round(((parseInt(color[0]) * 299) + (parseInt(color[1]) * 587) + (parseInt(color[2]) * 114)) / 1000);
+    return brightness < 125;
 }
