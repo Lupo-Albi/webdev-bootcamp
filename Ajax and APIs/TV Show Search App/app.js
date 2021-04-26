e02e2293e4165a79000718cf333e460f2f823127
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-04-26 19:22:15 -0300

    Finished TV Show Search APP class

--- Ajax and APIs/TV Show Search App/app.js
+++ Ajax and APIs/TV Show Search App/app.js
@@ -1,6 +1,24 @@
 const form = document.querySelector('#searchForm');
 
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', async function(e) {
+    try {
     e.preventDefault();
-    console.log("SUBMITTED");
+    const searchTerm  = form.elements.query.value;
+    const config = { params: { q: searchTerm } }
+    const response = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+    makeImages(response.data);
+    form.elements.query.value = '';
+    } catch (e) {
+        console.log("Couldn't load results.", e);
+    }
 }) 
+
+const makeImages = (shows) => {
+    for (let entry of shows) {
+        if (entry.show.image) {
+            const img = document.createElement('IMG');
+            img.src = entry.show.image.medium;
+            document.body.append(img);
+        }
+    }
+}
