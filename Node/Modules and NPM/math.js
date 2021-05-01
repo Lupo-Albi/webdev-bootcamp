07836a9debcd46511de647faec3cb0cfddd67d74
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-05-01 13:42:17 -0300

    Working with module.exports

---
+++ Node/Modules and NPM/math.js
@@ -0,0 +1,24 @@
+const add = (x, y) => x + y;
+// module.exports.add = (x, y) => x + y;
+
+const PI = 3.14159;
+// module.exports.PI = 3.14159;
+
+const square = x => x * x;
+// module.exports.square = x => x * x;
+//  =========================
+// const math = {
+//     add: add,
+//     PI: PI,
+//     square: square
+// }
+// module.exports = math;
+//  =========================
+// module.exports.add = add;
+// exports.add = add;
+// module.exports.PI = PI;
+// exports.PI = PI;
+// module.exports.square = square;
+// exports.square = square;
+
+module.exports = { add, PI, square };
