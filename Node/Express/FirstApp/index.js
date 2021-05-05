3bf8b5fe1549290b7137f088c458a32647661301
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-05-05 19:20:24 -0300

    Express Routing Basics

--- Node/Express/FirstApp/index.js
+++ Node/Express/FirstApp/index.js
@@ -2,13 +2,41 @@ const express = require("express");
 // import express from "express";
 const app = express();
 // console.dir(app);
-app.use((req, res) => {
-    console.log("We got a new request!");
-    // console.dir(req)
-    // res.send("HELLO, WE GOT YOUR REQUEST! THIS IS A RESPONSE!");
-    // res.send({ color: 'red' });
-    res.send('<h1>This is my webpage</h1>');
+// We can't have a http request that gets more than one response
+// app.use((req, res) => {
+//     console.log("We got a new request!");
+//     // console.dir(req)
+//     // res.send("HELLO, WE GOT YOUR REQUEST! THIS IS A RESPONSE!");
+//     // res.send({ color: 'red' });
+//     res.send('<h1>This is my webpage</h1>');
+// });
+
+// /cats => 'meow'
+// /dogs => 'woof
+// '/'
+
+app.get('/', (req, res) => {
+    res.send('This is the homepage!')  ;
+})
+
+app.post('/cats', (req, res) => {
+    res.send('POST REQUEST TO /cast!!! THIS IS DIFFERENT THAN A GET REQUEST!!');
+})
+
+app.get('/cats', (req, res) => {
+    // console.log("CAT REQUEST!!!");
+    res.send('MEOW!!!');
 });
+
+app.get('/dogs', (req, res) => {
+    res.send('WOOF!');
+})
+
+// Don't put that first
+app.get('*', (req, res) => {
+    res.send(`I don't know that path!`)
+});
+
 app.listen(8080, () => {
     console.log("LISTENING ON PORT 8080!");
 });
