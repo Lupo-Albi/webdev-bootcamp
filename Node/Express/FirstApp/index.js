c9b0410cefabec85ca6e25648473d9ee9fa4d99e
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-05-05 18:56:06 -0300

    The Request and Response Objects

--- Node/Express/FirstApp/index.js
+++ Node/Express/FirstApp/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 // import express from "express";
 const app = express();
 // console.dir(app);
-app.use(() => {
+app.use((req, res) => {
     console.log("We got a new request!");
+    // console.dir(req)
+    // res.send("HELLO, WE GOT YOUR REQUEST! THIS IS A RESPONSE!");
+    // res.send({ color: 'red' });
+    res.send('<h1>This is my webpage</h1>');
 });
 app.listen(8080, () => {
     console.log("LISTENING ON PORT 8080!");
