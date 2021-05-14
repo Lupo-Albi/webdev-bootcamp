a85f7f91174db0806348c1f1c11f6f78eadc16c6
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-05-14 15:12:37 -0300

    EJS Interpolation Syntax

--- Templating/Configuring_Express_For_EJS/index.js
+++ Templating/Configuring_Express_For_EJS/index.js
@@ -6,9 +6,9 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views')); // Joins the path directory for this file with the folder /views, so we can run the app wherever we want in our pc
 
 app.get('/', (req, res) => {
-    res.render('home'); // file extension is optional if the engine is set. render assumes the views folder already exists in the folder we are, so we don't need to write the relative path, just the file name
-})
+	res.render('home'); // file extension is optional if the engine is set. render assumes the views folder already exists in the folder we are, so we don't need to write the relative path, just the file name
+});
 
 app.listen(3000, () => {
-    console.log("Listening on port 3000")
+	console.log('Listening on port 3000');
 });
