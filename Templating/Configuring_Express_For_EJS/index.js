8a622d008e9c9a25fed0289f9beae3df39ab3010
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-05-14 14:18:11 -0300

    Setting the Views Directory

--- Templating/Configuring_Express_For_EJS/index.js
+++ Templating/Configuring_Express_For_EJS/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path'); // Node built-in module
+
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views')); // Joins the path directory for this file with the folder /views, so we can run the app wherever we want in our pc
 
 app.get('/', (req, res) => {
-    res.render('home'); // file extension is optional if the engine is set. render assumes the views folder already exists, so we don't need to write the relative path, just the file name
+    res.render('home'); // file extension is optional if the engine is set. render assumes the views folder already exists in the folder we are, so we don't need to write the relative path, just the file name
 })
 
 app.listen(3000, () => {
