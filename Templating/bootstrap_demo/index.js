0d96c365168fc147ba9a06ea485050fd7350515a
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-05-18 20:35:11 -0300

    EJS and partials

--- Templating/bootstrap_demo/index.js
+++ Templating/bootstrap_demo/index.js
@@ -9,18 +9,18 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views')); // Joins the path directory for this file with the folder /views, so we can run the app wherever we want in our pc
 
 app.get('/', (req, res) => {
-	res.render('home'); // file extension is optional if the engine is set. render assumes the views folder already exists in the folder we are, so we don't need to write the relative path, just the file name
+	res.render('home', { title: 'Home' }); // file extension is optional if the engine is set. render assumes the views folder already exists in the folder we are, so we don't need to write the relative path, just the file name
 });
 
 app.get('/rand', (req, res) => {
 	const num = Math.floor(Math.random() * 10) + 1;
-	res.render('random', { rand: num });
+	res.render('random', { rand: num, title: 'random' });
 	// { num } == { num: num }
 });
 
 app.get('/cats', (req, res) => {
 	const cats = [ 'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston' ];
-	res.render('cats', { cats });
+	res.render('cats', { cats, title: 'Cats' });
 });
 
 app.get('/r/:subreddit', (req, res) => {
@@ -29,7 +29,7 @@ app.get('/r/:subreddit', (req, res) => {
 	if (data) {
 		res.render('subreddit', { ...data });
 	} else {
-		res.render('notfound', { subreddit });
+		res.render('notfound', { subreddit, title: 'Error' });
 	}
 });
 
