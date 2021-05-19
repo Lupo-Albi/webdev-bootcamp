17bc4091e11306b31bd2a47334f6eb42aa6f8ac6
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-05-18 21:10:25 -0300

    Parsing the request body

--- RESTful_routes/RESTdemo/index.js
+++ RESTful_routes/RESTdemo/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.json()); // for parsing application/json
+
 app.get('/tacos', (req, res) => {
 	res.send('GET /tacos response');
 });
 
 app.post('/tacos', (req, res) => {
-	res.send('POST /tacos responde');
+	console.log(req.body);
+	const { meat, qty } = req.body;
+	res.send(`OK here are your ${qty} ${meat} taco(s)`);
 });
 
 app.listen(3000, () => {
