a1ad1013ce5c66df4aaf33b2c8d2ea589e054d55
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-09-15 22:09:40 -0300

    Updating Products

--- Mongoose/Express and Mongoose/index.js
+++ Mongoose/Express and Mongoose/index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const Product = require('./models/product');
+const methodOverride = require('method-override');
 
 mongoose
 	.connect('mongodb://localhost:27017/farmStand', {
@@ -20,6 +21,7 @@ mongoose
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 const port = 3000;
 
 app.get('/products', async (req, res) => {
@@ -41,6 +43,18 @@ app.get('/products/:id', async (req, res) => {
 	const { id } = req.params;
 	const product = await Product.findById(id);
 	res.render('products/show', { product });
+});
+
+app.get('/products/:id/edit', async (req, res) => {
+	const { id } = req.params;
+	const product = await Product.findById(id);
+	res.render('products/edit', { product });
+});
+
+app.put('/products/:id', async (req, res) => {
+	const { id } = req.params;
+	const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+	res.redirect(`/products/${product._id}`);
 });
 
 app.listen(port, () => {
