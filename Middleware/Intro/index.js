e26c74cd179203b47582f13452cc2efb06320820
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-09-23 14:40:13 -0300

    Defining Our Own Middleware

--- Middleware/Intro/index.js
+++ Middleware/Intro/index.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
-app.use(
-    morgan('dev')
-)
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+    console.log('My first middleware');
+    return next();
+    console.log('This is my first middleware - after calling next');
+});
+app.use((req, res, next) => {
+    console.log('This is my second middleware');
+    return next();
+})
+app.use((req, res, next) => {
+    console.log('This is my third middleware');
+    return next();
+})
+
 
 app.get('/', (req, res) => {
     res.send('Home Page!');
