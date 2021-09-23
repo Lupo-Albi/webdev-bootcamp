132bdc3b53049eef25635cf5a36a9647f4784542
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-09-23 14:56:46 -0300

    More Middleware Practice

--- Middleware/Intro/index.js
+++ Middleware/Intro/index.js
@@ -2,27 +2,36 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
-app.use(morgan('dev'));
+app.use(morgan('tiny'));
+
 app.use((req, res, next) => {
-    console.log('My first middleware');
-    return next();
-    console.log('This is my first middleware - after calling next');
-});
-app.use((req, res, next) => {
-    console.log('This is my second middleware');
-    return next();
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
+    next();
 })
-app.use((req, res, next) => {
-    console.log('This is my third middleware');
-    return next();
-})
+
+// app.use((req, res, next) => {
+//     console.log('My first middleware');
+//     return next();
+//     console.log('This is my first middleware - after calling next');
+// });
+// app.use((req, res, next) => {
+//     console.log('This is my second middleware');
+//     return next();
+// })
+// app.use((req, res, next) => {
+//     console.log('This is my third middleware');
+//     return next();
+// })
 
 
 app.get('/', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`);
     res.send('Home Page!');
 });
 
 app.get('/dogs', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`);
     res.send('Woof woof!');
 });
 
