49ae4bec428ce92f33568f070f1cce5c33439e0c
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-09-23 14:24:28 -0300

    Using Morgan - Logger Middleware

---
+++ Middleware/Intro/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+app.use(
+    morgan('dev')
+)
+
+app.get('/', (req, res) => {
+    res.send('Home Page!');
+});
+
+app.get('/dogs', (req, res) => {
+    res.send('Woof woof!');
+});
+
+app.listen(3000,() => {
+    console.log('App is running on localhost:3000')
+});
