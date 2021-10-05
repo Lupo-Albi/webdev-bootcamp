1191f017adabb1f37f695309d2c96dc8972b8797
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-10-05 15:52:19 -0300

    Data Relationships With Mongo

---
+++ Mongo Relationships/mongo-relationships/Models/user.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+mongoose
+	.connect('mongodb://localhost:27017/relationshipDemo')
+	.then(() => {
+		console.log('Mongo Connection Open!');
+	})
+	.catch((err) => {
+		console.log('Oh no Mongo Connection Error!');
+		console.log(err);
+	});
+
+const userSchema = new mongoose.Schema({
+	first: String,
+	last: String,
+	addresses: [
+		{
+			_id: { id: false },
+			street: String,
+			city: String,
+			state: String,
+			country: String
+		}
+	]
+});
+
+const User = mongoose.model('User', userSchema);
+
+const makeUser = async () => {
+	const u = new User({
+		first: 'Harry',
+		last: 'Potter'
+	});
+	u.addresses.push({
+		street: '123 Sesame St.',
+		city: 'New York',
+		state: 'NY',
+		country: 'USA'
+	});
+	const res = await u.save();
+	console.log(res);
+};
+
+const addAddress = async (id) => {
+	const user = await User.findById(id);
+	user.addresses.push({
+		street: '99 3rd St.',
+		city: 'New York',
+		state: 'NY',
+		country: 'USA'
+	});
+	const res = await user.save();
+	console.log(res);
+};
+
+addAddress('615ba48e979705d4caf4921a');
+
+// makeUser();
